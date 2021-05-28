b3c1e4163a762ba4a571c25d93777eb861442048
Temilayo Subair <olajnrsubair@gmail.com>
2021-05-28 18:02:53 +0100

    removed version for postgres driver

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "anlysis"
+set :application, "analysis"
 set :repo_url, "git@github.com:Fersubair40/analysis-api.git"
 set :branch, 'main'
 
